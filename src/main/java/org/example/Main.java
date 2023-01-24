4e614fb83b1ce07e0af9ae82c797243dacfcb10f
Iker <idelhoyo007@ikasle.ehu.eus>
2023-01-24 18:25:47 +0100

    Versión Definitiva

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Una versión alternativa");
+        System.out.println("Este es mi mensaje definitivo");
     }
 }
