e439aa6beffb10a42df84b4b20815d29031f726a
Iker <idelhoyo007@ikasle.ehu.eus>
2023-01-24 18:23:59 +0100

    Versión Alternativa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mi primer mensaje");
+        System.out.println("Una versión alternativa");
     }
 }
